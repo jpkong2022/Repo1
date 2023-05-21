ad4942d38cb3d55452ed598a34d56637ecea724b
jpkong2022 <99097145+jpkong2022@users.noreply.github.com>
2023-05-21 12:37:23 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "ami" { }
-variable "instance_type" { }
-variable "instance_count" { }
-variable "region" { }
-variable "access_key" { }
-variable "secret_key" { }
-variable "sgname" { }
+variable "ami" {default ami = "ami-0c9978668f8d55984" }
+variable "instance_type" {default instance_type = "t2.large" }
+variable "instance_count" {default instance_count = "1" }
+variable "region" {default region = "us-east-1"}
+variable "access_key" {default access_key = "AKIARRWBGNQBHXLZYAGY"}
+variable "secret_key" {default secret_key = "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"}
+variable "sgname" {default sgname = "default" }
