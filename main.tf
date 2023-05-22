87d51501a88197dd6ca3381018c9d303fae6da60
jpkong2022 <99097145+jpkong2022@users.noreply.github.com>
2023-05-21 21:54:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
 terraform {
-backend  "http" {
-address = "https://us1-smax.saas.microfocus.com/iac-controller/v1/393740922/terraform/Workspace1/backend"
-lock_address = "https://us1-smax.saas.microfocus.com/iac-controller/v1/393740922/terraform/Workspace1/backend/lock"
-unlock_address = "https://us1-smax.saas.microfocus.com/iac-controller/v1/393740922/terraform/Workspace1/backend/lock"
-lock_method = "POST"
-unlock_method ="DELETE"
-username = "jean-patrick.kong@microfocus.com"
-password = "Automation.123"
-skip_cert_verification = "true"
-}
-  required_version = "~> 1.0" 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
